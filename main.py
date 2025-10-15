265e4fc5c2bf9c163b18c7ec0b7c03748e64441a
Juan Diaz <diaz.juan@insercor.com>
2025-10-15 17:46:08 -0500

    refactor main.py to comment out unused model imports and initialization

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 # Importando elementos
-from models.local_model import model_local_process
+#from models.local_model import model_local_process
+# from models.langchain_ollama_model import model_langchain_process
 from models.api_model import model_api_process
-from models.langchain_ollama_model import model_langchain_process
 from frontend.chatbot import init_chatbot
 
 def main():
     # Ejecutando proceso completo
-    init_chatbot( execute_model=model_langchain_process )
-    
-    #init_chatbot( execute_model=model_local_process )
-    #init_chatbot( execute_model=model_api_process )
+    # init_chatbot( execute_model=model_langchain_process )
+    # init_chatbot( execute_model=model_local_process )
+    init_chatbot( execute_model=model_api_process )
 
 if __name__ == "__main__":
     main()
