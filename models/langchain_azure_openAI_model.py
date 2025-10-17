ca8969385ce6fa689a9c2af69683ce65408454f4
MateoOlayaA <mateo.olaya.aricapa@gmail.com>
2025-10-17 17:17:27 -0500

    variables entorno organizadas y langchain con azureOpenAI

--- models/langchain_azure_openAI_model.py
+++ models/langchain_azure_openAI_model.py
@@ -1,8 +1,8 @@
 # Importando librerías
-from openai import (AzureOpenAI)
-from pathlib import Path
 from settings.envs import get_envs
+from langchain_openai import AzureChatOpenAI
 
+from pathlib import Path
 import os
 import json
 
@@ -121,42 +121,36 @@ Debes dar la respuesta en formato markdown, usa listas, tablas, emojis, enlaces,
 - **NO INVENTES** información, si no sabes dirige al usuario al enlace mas coherente
 """
 
-def model_api_process( input ):
+def langchain_azureOpenAI_model( input ):
     """
-        Esta función ejecuta todo el proceso
-        API de un modelo privado para empezar
-        a interactuar
-    """
+        Esta función ejecuta la lógica para
+        interactuar con un modelo de OpenAI
+        desde el entorno de Azure.
 
-    # Configurando modelo openAI que está desplegado en Azure
-    client = AzureOpenAI(
-        api_version=envs["version"],     
+        Documentación: https://python.langchain.com/api_reference/openai/chat_models/langchain_openai.chat_models.azure.AzureChatOpenAI.html#langchain_openai.chat_models.azure.AzureChatOpenAI
+    """
+    # Configurando modelo
+    llm = AzureChatOpenAI(
+        azure_deployment=envs["deployment"],
+        api_version=envs["version"],
+        api_key=envs["api_key"],
         azure_endpoint=envs["endpoint"],
-        api_key=envs["api_key"], 
+        #temperature=0,
+        max_tokens=16384,
+        #timeout=None,
+        #max_retries=2,
+        model="gpt-5-nano",
+        #model_version="",
     )
-    
+
     retrieved_context = FIXED_CONTEXT
 
-    # Contenido para el modelo
-    messages=[
-        {
-            "role": "system",
-            "content": MASTER_PROMPT,
-        },
-        {
-            "role": "system",
-            "content": f"---\n# Contexto recuperado para respuestas:\n{ retrieved_context }",
-        },
-        {
-            "role": "user",
-            "content": input,
-        },   
+    messages = [
+        ("system", MASTER_PROMPT),
+        ("system", f"---\n# Contexto recuperado para respuestas:\n{ retrieved_context }"),
+        ("human", input),
     ]
 
-    response = client.chat.completions.create(
-        messages=messages,
-        max_completion_tokens=16384,
-        model=envs["deployment"],
-    )
-    
-    return response.choices[0].message.content
+    response_model = llm.invoke( messages )
+
+    return response_model.content
