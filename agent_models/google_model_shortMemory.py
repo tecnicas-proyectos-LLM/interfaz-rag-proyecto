5a0b9f381dbcd49a46c3ff20c55a343debeb59ec
Juan Diaz <diaz.juan@insercor.com>
2025-11-06 14:41:46 -0500

    extract get_setting_model to new file

--- agent_models/google_model_shortMemory.py
+++ agent_models/google_model_shortMemory.py
@@ -2,80 +2,20 @@
 from settings.envs import get_envs
 
 # Importando librerías langchain
-from langchain.agents import create_agent
-from langchain.chat_models import init_chat_model
 from langchain.agents.middleware import SummarizationMiddleware
 from langgraph.checkpoint.postgres import PostgresSaver 
 
 # Importando Tools y Prompt para el modelo
 from tools.tools import ModelTools, prompt_with_context
 from system_prompts.master_prompt import MASTER_PROMPT
+from langchain.agents import create_agent
+from agent_models.model_config import models
 
 # Cargando variables de entorno
 envs = get_envs()
 
 # -----------------------------------------------------------------------------
 # FUNCIONES PARA CONFIGURACIÓN
-# -----------------------------------------------------------------------------
-def get_setting_model():
-    """
-        Esta función se encarga de cargar y configurar
-        dos modelos que se usarán en el proceso del RAG.
-
-        - Model RAG: es el modelo que se usará para aplicar
-        el proceso de RAG.
-
-        - Model summary: es el modelo que se usará para aplicar
-        la técnica summary con tal de resumir toda el hilo
-        conversacional que se lleva con el modelo. 
-    """
-    model_RAG = init_chat_model(
-        f"google_genai:{ envs["google_model_ia"] }",
-        api_key=envs["api_key_google"],
-        #temperature=0.5,
-        #timeout=10,
-        #max_tokens=1000,
-    )
-
-    model_summary = init_chat_model(
-        f"google_genai:{ envs["google_model_ia"] }",
-        api_key=envs["api_key_google"],
-    )
-
-    return {
-        "model_RAG"    : model_RAG,
-        "model_summary": model_summary,
-    }
-
-def normalize_content( message ):
-    """
-        Gemini puede regresar respuestas de tipo texto, imágenes, entre otros.
-        Por esa razón es necesario organizar la información para que extraer
-        el contenido que se mostrará en pantalla. En este caso, sólo se
-        trabajará con texto.
-    """
-    content = message.content
-
-    # Si la respuesta es una lista
-    if isinstance( content, list ):
-        response = content[0]["text"]
-        return response
-    
-    # Si la respuesta es directamente una cadena
-    elif isinstance( content, str ):
-        return content
-    
-    # Si no hay respuesta del modelo
-    else:
-        return "No hubo respuesta"
-
-# -----------------------------------------------------------------------------
-# PROCESO PRINCIPAL
-# -----------------------------------------------------------------------------
-
-# Obteniendo modelos LLM para todo el proceso
-models = get_setting_model()
-
 def agent_google_shortMemory( input, thread_id ):
     """
         Esta función contiene toda la lógica
@@ -102,7 +42,7 @@ def agent_google_shortMemory( input, thread_id ):
                     max_tokens_before_summary=4000,
                     messages_to_keep=20,
                 ),
-                #prompt_with_context, # Añadiendo proceso para aplicar RAG
+                prompt_with_context, # Añadiendo proceso para aplicar RAG
             ],
             checkpointer=checkpointer,
         )
@@ -116,4 +56,29 @@ def agent_google_shortMemory( input, thread_id ):
         response_model = normalize_content( message=result["messages"][-1] )
 
         return response_model
+
+def normalize_content( message ):
+    """
+        Gemini puede regresar respuestas de tipo texto, imágenes, entre otros.
+        Por esa razón es necesario organizar la información para que extraer
+        el contenido que se mostrará en pantalla. En este caso, sólo se
+        trabajará con texto.
+    """
+    content = message.content
+
+    # Si la respuesta es una lista
+    if isinstance( content, list ):
+        response = content[0]["text"]
+        return response
     
+    # Si la respuesta es directamente una cadena
+    elif isinstance( content, str ):
+        return content
+    
+    # Si no hay respuesta del modelo
+    else:
+        return "No hubo respuesta"
+
+# -----------------------------------------------------------------------------
+# PROCESO PRINCIPAL
+# -----------------------------------------------------------------------------
---
+++ agent_models/model_config.py
@@ -0,0 +1,36 @@
+# Importando librerías langchain
+from langchain.chat_models import init_chat_model
+
+# Importando variables de entorno
+from settings.envs import get_envs
+envs = get_envs()
+
+def get_setting_model():
+    """
+        Esta función se encarga de cargar y configurar
+        dos modelos que se usarán en el proceso del RAG.
+
+        - Model RAG: es el modelo que se usará para aplicar
+        el proceso de RAG.
+
+        - Model summary: es el modelo que se usará para aplicar
+        la técnica summary con tal de resumir toda el hilo
+        conversacional que se lleva con el modelo. 
+    """
+    model_RAG = init_chat_model(
+        f"google_genai:{ envs["google_model_ia"] }",
+        api_key=envs["api_key_google"],
+    )
+
+    model_summary = init_chat_model(
+        f"google_genai:{ envs["google_model_ia"] }",
+        api_key=envs["api_key_google"],
+    )
+
+    return {
+        "model_RAG"    : model_RAG,
+        "model_summary": model_summary,
+    }
+
+# Obteniendo modelos LLM para todo el proceso
+models = get_setting_model()
