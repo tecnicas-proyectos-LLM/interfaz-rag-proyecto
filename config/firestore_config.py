5648902f707ce159b1b65446a7f7451f1d7453c1
Juan Diaz <diaz.juan@insercor.com>
2025-11-06 17:01:14 -0500

    add firebase config

---
+++ config/firestore_config.py
@@ -0,0 +1,27 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+import os
+
+# Inicializar Firebase (solo una vez)
+def initialize_firebase():
+    """Inicializa Firebase con las credenciales"""
+    if not firebase_admin._apps:
+        cred = credentials.Certificate({
+            "type": "service_account",
+            "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+            "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+            "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+            "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+            "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL")
+        })
+        
+        firebase_admin.initialize_app(cred)
+
+def get_firestore_client():
+    """Retorna el cliente de Firestore"""
+    initialize_firebase()
+    return firestore.client()
