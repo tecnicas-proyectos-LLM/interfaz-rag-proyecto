9f836754bba732245d508e40cf1a74c1f3e4812a
MateoOlayaA <mateo.olaya.aricapa@gmail.com>
2025-11-12 12:51:45 -0500

    changes for API

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 # Importando elementos
-#from chat_models.langchain_googleAI_mode import model_langchain_google_process
 from agent_models.google_model_shortMemory import agent_google_shortMemory
-
-from frontend.chatbot import init_chatbot
-#from settings.database_connection import check_connection
+from helpers.uuid import generatorUUID
 
 def main():
-    # Ejecutando proceso completo
-    #init_chatbot( execute_model=model_langchain_google_process )
     
-    init_chatbot( execute_model=agent_google_shortMemory )
+    agent_google_shortMemory( input="Texto", thread_id=generatorUUID() )
 
 if __name__ == "__main__":
     main()
